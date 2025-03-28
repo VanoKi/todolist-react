e945acf0dd3d0b35011184e7c3f9c9ea3ae3aa2e
VanoKi <kirsvano@gmail.com>
2025-03-28 19:27:51 +0300

    feat: add items to tasks1

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,14 @@ const App = () => {
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
     { id: 4, title: 'Redux', isDone: false },
+    { id: 5, title: 'TypeScript', isDone: false },
+    { id: 6, title: 'RTK query', isDone: false },
   ]
 
   const tasks2: Task[] = [
     { id: 1, title: 'Hello world', isDone: true },
     { id: 2, title: 'I am Happy', isDone: false },
     { id: 3, title: 'Yo', isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
   ]
   return <div className="app">
     <TodoListItem title={'What to learn'} tasks={tasks1} date={String(Day.toLocaleDateString())}/>
