980aebddcc4c74f283df6a7d9d88f7320ea96f52
VanoKi <kirsvano@gmail.com>
2025-03-28 20:33:28 +0300

    feat: del second list and items

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,8 @@ const App = () => {
     { id: 5, title: 'TypeScript', isDone: false },
     { id: 6, title: 'RTK query', isDone: false },
   ]
-
-  const tasks2: Task[] = [
-    // { id: 1, title: 'Hello world', isDone: true },
-    // { id: 2, title: 'I am Happy', isDone: false },
-    // { id: 3, title: 'Yo', isDone: false },
-  ]
   return <div className="app">
     <TodoListItem title={'What to learn'} tasks={tasks1} date={String(Day.toLocaleDateString())}/>
-    <TodoListItem title={'Fraces'} tasks={tasks2} />
   </div>
 }
 
