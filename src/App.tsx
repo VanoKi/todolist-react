95ba89b81bdba0f56eaac62c3fb417ea1d0d8feb
VanoKi <kirsvano@gmail.com>
2025-03-31 17:11:48 +0300

    feat: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {TodoListItem} from "./TodoListItem.tsx";
+import {useState} from "react";
 const Day = new Date()
 
 export type Task = {
@@ -9,20 +10,20 @@ export type Task = {
 }
 
 const App = () => {
-  let tasks: Task[] = [
+  const [tasks, setTasks]= useState<Task[]>([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
     { id: 4, title: 'Redux', isDone: false },
     { id: 5, title: 'TypeScript', isDone: false },
     { id: 6, title: 'RTK query', isDone: false },
-  ]
+  ])
 
   function deleteTask(taskId: number) {
-    tasks = tasks.filter(task => {
+    const filtredTasks = task.filter( task => {
       return task.id !== taskId
     })
-    console.log(tasks)
+    setTasks(filtredTasks)
   }
 
   return <div className="app">
