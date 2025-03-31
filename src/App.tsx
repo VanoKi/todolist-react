2d8ea56c8ab46a39e676d0e8e6d5784c95c91469
VanoKi <kirsvano@gmail.com>
2025-03-31 19:13:39 +0300

    fix: console.log

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import './App.css'
+import './App.css';
 import {TodoListItem} from "./TodoListItem.tsx";
 import {useState} from "react";
+
 const Day = new Date()
 
 export type Task = {
@@ -10,6 +11,7 @@ export type Task = {
 }
 
 const App = () => {
+
   const [tasks, setTasks]= useState<Task[]>([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
@@ -19,10 +21,19 @@ const App = () => {
     { id: 6, title: 'RTK query', isDone: false },
   ])
 
+  const [filter, setFilter] = useState('all')
+
+  let filtredTasks = tasks
+  if (filter === 'active') {
+    filtredTasks = tasks.filter(task => !task.isDone)
+  }
+  if (filter === 'completed') {
+    filtredTasks = tasks.filter(task => task.isDone)
+  }
+
   function deleteTask(taskId: number) {
-    const filtredTasks = tasks.filter( task => {
-      return task.id !== taskId
-    })
+    const filtredTasks = tasks.filter( task => task.id !== taskId)
+    console.log(filtredTasks)
     setTasks(filtredTasks)
   }
 
